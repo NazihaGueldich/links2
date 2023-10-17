83694602044f889d408eb07b21139e3f230b075d
NazihaGueldich <gueldichnaziha@gmail.com>
2023-10-17 10:05:14 +0200

    pag 2 part 1.1

--- resources/views/page2/page2.blade.php
+++ resources/views/page2/page2.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap Footer Example</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pg2.css') }}">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" />
     <link rel="stylesheet" href="{{ asset('js/scriptpj2.js') }}">
 
