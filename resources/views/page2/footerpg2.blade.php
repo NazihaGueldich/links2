4ceffe74c0502f956d193e9190b4b320f77220e4
NazihaGueldich <gueldichnaziha@gmail.com>
2023-10-19 01:31:09 +0200

    footer pg 2

--- resources/views/page2/footerpg2.blade.php
+++ resources/views/page2/footerpg2.blade.php
@@ -1,5 +1,68 @@
 </div>
+<footer class="foot mt-5">
+    <div class="footer-column">
+        <div class="left-content">
+            <h3 id='h3ft' >튀니지에 대해서</h3>
+            <ul id='ulft' >
+                <li><a href="#">역사</a> |</li>
+                <li><a href="#">음식</a> |</li>
+                <li><a href="#"><strong>문화</strong></a> |</li>
+                <li><a href="#">목적지</a> |</li>
+                <li><a href="#">활동</a> |</li>
+                <li><a href="#">경제</a> |</li>
+                <li><a href="#">업적</a></li>
+            </ul>
+        </div>
 
+        <div  class="right-content">
+            <h3 id='h3ft'> 여행 정보</h3>
+            <ul id='ulft2'>
+
+                <li><a href="#">공항</a> |</li>
+                <li><a href="#">호텔</a> |</li>
+                <li><a href="#">통화</a> |</li>
+                <li><a href="#">계절</a></li>
+                <bR>
+                <li class="ml-5"><a href="#">최고의 제안</a> |</li>
+                <li><a href="#"><strong>우리가 도울 수 있는 방법</strong></a> |</li>
+                <li><a href="#">비상</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+
+    <div class="footer-images" >
+        <div class='divft' >
+            <img src="{{ asset('images/Group (3).png') }}" class='imgft'>
+            <img src="{{ asset('images/Group (4).png') }}" class='imgft'>
+            <img src="{{ asset('images/Group 52.png') }}" class='imgft'>
+        </div>
+        <div class='divft' >
+            <img src="{{ asset('images/Group 62.png') }}" class='imgft'>
+            <img src="{{ asset('images/Group 63.png') }}" class='imgft'>
+            <img src="{{ asset('images/Group 39589.png') }}" class='imgft'>
+        </div>
+        <div>
+            <div class="footer-images2">
+                <span class="titr">All rights
+                    reserved</span>
+                <hr class="lignes ml-1 mr-3 lnpgft"  >
+                <div class="img12" >
+                    <img src="{{ asset('images/Rectangle2.svg') }}">
+                    <img src="{{ asset('images/MicrosoftTeams-image.png') }}" style="height: 24px;">
+                    <img src="{{ asset('images/MicrosoftTeams-image_(1).png') }}" style="height: 24px;">
+                </div>
+            </div>
+            <div class="bloc2 ml-5">
+                <img src="{{ asset('images/fb.svg') }}" style="height: 18px; max-width: 17px; margin-right: 4%;">
+                <img src="{{ asset('images/ig.svg') }}" style="height: 18px; max-width: 17px; margin-right: 4%;">
+                <img src="{{ asset('images/yt.svg') }}" style="height: 18px; max-width: 26px;">
+            </div>
+        </div>
+    </div>
+</footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
