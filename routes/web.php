a969b5f2c68fe492630494aa254857b69b0bc403
NazihaGueldich <gueldichnaziha@gmail.com>
2023-10-23 10:27:12 +0200

    auth + squelette pg blog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/1', function () {
-    return view('page1.page1');
-});
-Route::get('/2', function () {
-    return view('page2.page2');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/3', function () {
-    return view('page3.page3');
-});
+require __DIR__.'/auth.php';
