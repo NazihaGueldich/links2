ae0b040b96cc83a8eeae52e6716505c7aae515a7
NazihaGueldich <gueldichnaziha@gmail.com>
2023-10-28 19:59:42 +0200

    retouche

--- public/js/atlantis.js
+++ public/js/atlantis.js
@@ -73,7 +73,7 @@ function srllcrds(x){
     var h1F0=['Kamel Lagha','Heidi Jung','Meriem Zoghlami','Sonia Ben Amor'];
     var pst0=['CEO of the agency','Korean department','executive management','Tiketing Department'];
     var eml0=['Kamel@atlantis.tn','jh.heidi.j@gmail.com','atlantis@atlantis.tn','Sonia.Benamor@atlantis.tn'];
-    var img0=['crd1frst.jpg','crd2frst.jpg','crd3frst.jpg','crd4frst.jpg'];
+    var img0=['crd1frst.png','crd2frst.jpg','crd3frst.jpg','crd4frst.jpg'];
     //seconde crcl
     var h1K1=['Zhouhour','정지혜','마리엠 조그라미','소니아 벤 아모르'];
     var h1F1=['Kamel Lagha','Heidi Jung','Meriem Zoghlami','Sonia Ben Amor'];
