ecc6a5e203cf353cb00ea49eb4365b8b1edebc57
SADOK <chihasadok@gmail.com>
2023-10-30 00:59:31 +0100

    fix nav pg1 2

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-
+<link rel="icon" href="/Tunisiatrip.ico" type="image/x-icon">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>TunisiaTrip - Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
