7f85a235df8d4d312bda85e3eb7e10d9471c5921
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-01 13:03:23 +0100

    bdalt il fivicon bfivicone o5re

--- resources/views/page2/scriptpg2.blade.php
+++ resources/views/page2/scriptpg2.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="UTF-8">
-    <link rel="icon" href="/Tunisiatrip.ico" type="image/x-icon">
+    <link rel="icon" href="/TunisiaTrip_Favicon.svg" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
