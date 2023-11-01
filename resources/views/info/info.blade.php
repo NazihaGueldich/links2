4c2df3ac93a507d7a265547c6e17c46a363825ae
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-01 09:43:36 +0100

    time on page info

--- resources/views/info/info.blade.php
+++ resources/views/info/info.blade.php
@@ -498,4 +498,30 @@
             <div class="my-5"></div>
         </div>
     </main>
+    <script>
+        /* navbar */
+function updateClock() {
+    const tunisTime = new Date();
+
+    let hours = tunisTime.getHours();
+    const minutes = tunisTime.getMinutes();
+    const seconds = tunisTime.getSeconds();
+
+    let amOrPm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    const timeString =
+        `${(hours < 10 ? '0' : '') + hours}:${(minutes < 10 ? '0' : '') + minutes}:${(seconds < 10 ? '0' : '') + seconds} ${amOrPm}`;
+
+    const tunisTimeElement = document.getElementById("tunis-time");
+    if (tunisTimeElement) {
+        tunisTimeElement.textContent = timeString;
+    }
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
+    </script>
   @include('layouts.front.footer')
