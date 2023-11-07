216bc8f7c78b4a74e346a52a1893d0bc8257d1eb
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-07 12:26:25 +0100

    page atlantis :session 6 :change img

--- resources/views/atlantis/atlantis.blade.php
+++ resources/views/atlantis/atlantis.blade.php
@@ -544,7 +544,7 @@
                
                 <div class="col-lg-3 offset-lg-1 sess6crd radius-20">
                     <div class="mask-image">
-                        <img class="sess6crdimg" src="{{ asset('images/guesthouse-_bizerte-_dar_Ellamma.jpg') }}" class="img-fluid"
+                        <img class="sess6crdimg" src="{{ asset('images/dar_bel_gesem.jpg') }}" class="img-fluid"
                             alt="Carthage">
                     </div>
                     <img src="{{ asset('images/sess6crd1crcl.png') }}" class="cercle-img">
@@ -573,7 +573,7 @@
                 </div>
                 <div class="col-lg-3  sess6crd radius-20">
                     <div class="mask-image">
-                        <img class="sess6crdimg" src="{{ asset('images/dar_bel_gesem.jpg') }}" class="img-fluid"
+                        <img class="sess6crdimg" src="{{ asset('images/guesthouse-_bizerte-_dar_Ellamma.jpg') }}" class="img-fluid"
                             alt="Carthage">
                     </div>
                     <img src="{{ asset('images/sess6crd1crcl.png') }}" class="cercle-img">
