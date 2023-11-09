49992d8b6ae91b3eb1dc9379790feb36c756b7a7
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-09 16:53:55 +0100

    page subpage: section 3

---
+++ public/js/subpage.js
@@ -0,0 +1,21 @@
+$('#section3 .slick-section3-web').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear'
+});
+
+$('#section3 .slick-section3-mobil').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear'
+});
--- resources/views/pages/subpage.blade.php
+++ resources/views/pages/subpage.blade.php
@@ -23,9 +23,276 @@
                 <button type="button"
                     class="btn btn-outline-primary d-flex align-items-center btnsect2 radius-20 pr-lg-4 pl-lg-4">
                     <img src="{{ asset('images/enveloppe.png') }}" alt="Enveloppe">문의하기</button>
-
             </section>
         </div>
+    </div>
+    <div class="container">
+        <section id='section3'>
+            <h2 class="main-title text-center">How to get from korea to tunisia</h2>
+            <p class="text-center">한국에서 튀니지로 여행하는 주요 3가지 방법은 다음과 같습니다</p>
+            <div id='slick-section3-web' class="slick-section3-web">
+                <div class="px-3" >
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Qatar Airways</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong>한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">도하<strong>카타르</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Turkish airlines</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong> 한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">도하 <strong>  카타르</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Airfrance</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong> 한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">파리 <strong>프랑스</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong>  튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Emirates</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천 <strong>한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">두바이 <strong>(아랍에미리트)</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스 <strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id='slick-section3-mobil' class="slick-section3-mobil">
+                <div class="px-3" >
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Qatar Airways</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong>한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">도하<strong>카타르</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Turkish airlines</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong> 한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">도하 <strong>  카타르</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Airfrance</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천<strong> 한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">파리 <strong>프랑스</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">5 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스<strong>  튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="px-3">
+                    <div class="crdsess3">
+                        <div class="crdsess3prt1 m-3">
+                            <p class="sub-sub-title text-center">Emirates</p>
+                        </div>
+                        <div class="crdsess3prt2">
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">인천 <strong>한국</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">두바이 <strong>(아랍에미리트)</strong></p>
+                            </div>
+                            <div class="col-3 p-0 route">
+                                <hr class="lignedisc">
+                                <div >
+                                    <img src="{{ asset('images/miniavion.png') }}" alt="Avion">
+                                    <p class="mb-0">4 hours</p>
+                                </div>
+                            </div>
+                            <div class="col-2 p-0">
+                                <p class="mb-0 pb-0">튀니스 <strong> 튀니지</strong></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ligne-separtion"></div>
+
+        </section>
     </div>
 
     @include('layouts.front.footer')
