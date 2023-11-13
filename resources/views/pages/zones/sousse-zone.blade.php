a1e41231af73ddf3608496ce54d9d6651b2a0def
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-13 17:33:47 +0100

    subpage:section 4

--- resources/views/pages/zones/sousse-zone.blade.php
+++ resources/views/pages/zones/sousse-zone.blade.php
@@ -8,7 +8,7 @@
 </div>
 <div class="zone-long-description">
     <div class="top-long-description">
-        <h2 class="main-title">수스</h2>
+        <h2 class="sub-title">수스</h2>
         <p>지중해 해안에 위치한 수스는 휴양도시로 유명해요. 해안을 가로지르는 긴 해변가에 고급 리조트는 물론, 레스토랑, 호텔, 쇼핑센터 등이 잘 갖춰져있어 튀니지 관광 중심지 중 하나로 꼽혀요. 특히 수스의 메디나는 유네스코 세계 문화 유산으로 지정됐을 정도로, 마그레브 이슬람 건축물의 전형적인 모습을 잘 보여주는 관광명소입니다. 수스 메디나에 방문하여 수스 고고학 박물관과 카스바, 수스 대 모스크 등을 구경해보세요.</p>
         <div class="wrap-btn-action">
             <span href="#" class="tag-info">Lorem tag 1</span>
