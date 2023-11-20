73e70f2569a24deaa5853794acd89018d8b06ab1
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-20 16:19:54 +0100

    page linksstation: part 1

--- resources/views/pages/linksstation.blade.php
+++ resources/views/pages/linksstation.blade.php
@@ -43,6 +43,11 @@
         </div>
     </nav>
 
+    <div class="container">
+        <section id='section-cont'>
+            <img  src="{{ asset('images/pap.png') }}" alt=''>
+        </section>
+    </div>
 
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
