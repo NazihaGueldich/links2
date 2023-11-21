1635ecf4d2ac528408718fbcbb7720fc3bef0ef8
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-21 22:53:01 +0100

    page subpage; learntag

--- resources/views/pages/zones/dogga-zone.blade.php
+++ resources/views/pages/zones/dogga-zone.blade.php
@@ -14,10 +14,7 @@
 
 튀니스에서 105km 남서쪽에 있는 두가는 한 때 번영했던 고대 로마의 도시 중 하나예요,. 두가의 로마유적은 북아프리카에서 가장 잘 보존된 것으로 꼽힙니다. 수천 년의 세월이 담긴 역사성과 보존의 탁월함을 인정받아 1997년 유네스코 문화유산에 등재되었고요. 의사당과 포럼, 원형 극장과 공중목욕탕 등 로마 제국 변방에 있던 작은 도시의 모습을 보여주는 인상적인 유적들을 확인할 수 있습니다.
         </p>
-        <div class="wrap-btn-action">
-            <span href="#" class="tag-info">Lorem tag 1</span>
-            <span href="#" class="tag-info">Lorem tag 1</span>
-        </div>
+        
     </div>
     <div class="bottom-long-description">
         <div class="col-lg-12  one-time-slide">
--- resources/views/pages/zones/sousse-zone.blade.php
+++ resources/views/pages/zones/sousse-zone.blade.php
@@ -10,10 +10,7 @@
     <div class="top-long-description">
         <h2 class="sub-title">수스</h2>
         <p>지중해 해안에 있는 수스는 휴양도시로 유명해요. 해안을 가로지르는 긴 바닷가에 고급 리조트는 물론, 레스토랑, 호텔, 쇼핑센터 등이 잘 갖춰져 있어 튀니지 관광 중심지 중 하나로 꼽혀요. 특히 수스의 메디나는 유네스코 세계 문화유산으로 지정됐을 정도로, 마그레브 이슬람 건축물의 전형적인 모습을 잘 보여주는 관광명소입니다. 수스 메디나에 방문하여 수스 고고학 박물관과 카스바, 대 모스크 등을 구경해 보세요.</p>
-        <div class="wrap-btn-action">
-            <span href="#" class="tag-info">Lorem tag 1</span>
-            <span href="#" class="tag-info">Lorem tag 1</span>
-        </div>
+       
     </div>
     <div class="bottom-long-description">
         <div class="col-lg-12  one-time-slide">
