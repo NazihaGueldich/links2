8a7fda29c70ca5cfc94cbc4cd32d27f22dfaffdc
NazihaGueldich <gueldichnaziha@gmail.com>
2023-11-21 18:27:35 +0100

    map crcls ytna7aw

--- resources/views/pages/zones/sousse-zone.blade.php
+++ resources/views/pages/zones/sousse-zone.blade.php
@@ -18,13 +18,7 @@
     <div class="bottom-long-description">
         <div class="col-lg-12  one-time-slide">
             <img src="{{ asset('images/Sousse-tunisiatrip.jpg') }}" class="w-100" id='imgsess1' alt="Sousse">
-            <div class="d-flex sccgrp p-lg-3 sess1direct align-items-center" id="dirct">
-                <img src="{{ asset('images/flr.png') }}" {{-- onclick="SPChangImgSess5(-1)" --}} alt="Arrow left">
-                <img id='sess1crc1' src="{{ asset('images/pt.png') }}" alt="Point">
-                <img id='sess1crc2' src="{{ asset('images/crcl.png') }}" alt="Cercle">
-                <img id='sess1crc3' src="{{ asset('images/crcl.png') }}"  alt="Cercle">
-                <img src="{{ asset('images/fll.png') }}" {{-- onclick="SPChangImgSess5(1)" --}} alt="Arrow Right">
-            </div>
+           
         </div>
     </div>
 </div>
