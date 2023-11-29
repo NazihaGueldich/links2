3c221778c881963b2cc7696333075976c3494653
zouhour <zourouabdelii@gmail.com>
2023-11-29 13:55:05 +0100

    home first section

--- resources/views/page1/page1.blade.php
+++ resources/views/page1/page1.blade.php
@@ -241,7 +241,7 @@
                     <div class="links_box-1">
 
                     </div>
-                    <div class="height-p-card">
+                    <div class="height-p-card mt-lg-3">
                         <button type="button" class="btn btn-outline-primary btn1-b">메디나
                             역사지구</button>
                         <p class="p1prt7 ">메디나 전통시장에서 역사적 정취를 느끼고,튀니지의 문화를 체험해보세요. 이
@@ -253,7 +253,7 @@
                     <div class="links_box-2">
 
                     </div>
-                    <div class="height-p-card">
+                    <div class="height-p-card mt-lg-3">
                         <button type="button" class="btn btn-outline-primary btn1-b">튀니지 미식
                             체험</button>
                         <p class="p1prt7 ">튀니지 음식은 동서양 요리 양식이 혼합된 형태예요. 역사적으로 고대
@@ -266,7 +266,7 @@
                     <div class="links_box-3">
 
                     </div>
-                    <div class="height-p-card">
+                    <div class="height-p-card mt-lg-3">
                         <button type="button" class="btn btn-outline-primary btn1-b ">인기
                             목적지</button>
                         <p class="p1prt7 ">시디부사이드, 엘젬, 두가, 수스 등 매력이 각기 다른 인기 목적지가
@@ -279,7 +279,7 @@
                     <div class="links_box-4">
 
                     </div>
-                    <div class="height-p-card">
+                    <div class="height-p-card mt-lg-3">
                         <button type="button" class="btn btn-outline-primary btn1-b">즐길거리</button>
                         <p class="p1prt7 ">사하라사막에서의 낙타 타기 체험, 지중해 바다에서의 서핑 등 다양한
                             액티비티를 즐길
